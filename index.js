a824cd03cf0707e4e330a341e119bdc9560df116
Abdessamie Mezzache <141078830+mathelem@users.noreply.github.com>
2024-11-14 00:21:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function save() {
+    let b = count + " - "
+    saveEl.textContent += b
+    count = 0
+    countEl.textContent = count
+}
+
+
